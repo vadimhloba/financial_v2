b8b54e4402d7dad3b5e61ca4e149133d01a7a38f
Yosuphew <vadimirua1409@gmail.com>
2023-01-16 11:19:57 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,225 @@
+<template>
+
+  <header>
+		<div class="container">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	</header>
+
+	<section class="business-impacted">
+		<div class="container">
+			<h1></h1>
+			<p></p>
+			<button></button>
+		</div>
+	</section>
+
+	<section class="out-pholosophy">
+		<div class="container">
+			<div class="col-one">
+				<h1></h1>
+				<h2></h2>
+			</div>
+			<div class="col-two">
+				<p></p>
+			</div>
+			<div class="col-three">
+				<img src="">
+			</div>
+		</div>
+	</section>
+
+	<section class="done-for-you">
+		<div class="container">
+			<div class="col-one">
+				<h1></h1>
+			</div>
+			<div class="col-two">
+				<div class="row-one">
+					<h1></h1>
+				</div>
+				<div class="row-two">
+					<div>
+						<h2></h2>
+						<p></p>
+					</div>
+					<div>
+						<h2></h2>
+						<p></p>
+					</div>
+				</div>
+				<div class="row-three">
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="why-us">
+		<div class="container">
+
+		</div>
+	</section>
+
+	<section class="industries-helped">
+		<div class="container">
+			<div class="col-one">
+				<h1></h1>
+			</div>
+			<div class="col-two">
+				<div class="row-one">
+					<h2></h2>
+					<button></button>
+				</div>
+				<div class="row-two">
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+					<div>
+						<h3></h3>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="how-it-works">
+		<div class="container">
+
+		</div>
+	</section>
+
+	<section class="get-cash">
+		<div class="container">
+			<div class="col-one">
+				<p></p>
+				<h1></h1>
+				<button></button>
+			</div>
+		</div>
+		<col-two>
+			<div class="row-one">
+				<h2></h2>
+			</div>
+			<div class="row-two">
+				<div>
+					<h2></h2>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+				<div>
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+			<div class="row-three">
+				<div>
+					<h2></h2>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+				<div>
+					<h2></h2>
+					<p></p>
+				</div>
+			</div>
+		</col-two>
+	</section>
+
+	<section class="client-review">
+		<div class="container">
+			<div class="col-one">
+				<h1></h1>
+				<a href="#"></a>
+			</div>
+			<div class="col-two">
+
+			</div>
+		</div>
+	</section>
+
+	<section class="faq">
+		<div class="container">
+			<div class="col-one">
+				<img src="">
+			</div>
+			<div class="col-two">
+				<a href="#"></a>
+				<a href="#"></a>
+				<div class="tab-appear"></div>
+				<div class="tab-appear"></div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<div class="col-one">
+				<h1></h1>
+			</div>
+			<div class="col-two">
+				<div class="row-one">
+					<p></p>
+					<h2></h2>
+				</div>
+				<div class="row-one">
+					<p></p>
+					<h2></h2>
+				</div>
+				<div class="row-one">
+					<div>
+						<p></p>
+						<img src="">
+					</div>
+					<div>
+						<p></p>
+						<img src="">
+					</div>
+				</div>
+			</div>
+			<div class="col-three">
+				<div class="row-one">
+					<p></p>
+				</div>
+				<div class="row-two">
+					<a href="#"></a>
+					<a href="#"></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>

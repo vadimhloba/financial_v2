91e212612415bb679cc27040ee7ba31d037e6890
Yosuphew <vadimirua1409@gmail.com>
2023-01-17 13:40:58 +0200

    added some styles

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
 					</div>
 				</div>
 				<div class="number">
-					<p class="big">(855) 855-9365</p>
+					<p>(855) 855-9365</p>
 				</div>
 			</div>
 		</header>
@@ -30,7 +30,7 @@
 				<div class="bg-img">
 					<h1>Has your business been impacted by the restrictions and impacts of COVID-19?</h1>
 					<div class="column">
-						<p class="big">Our professionals do all the hard work so you can sleep at night knowing it was done right</p>
+						<p>Our professionals do all the hard work so you can sleep at night knowing it was done right</p>
 						<button>Check eligibility</button>
 					</div>
 				</div>
@@ -57,14 +57,35 @@
 		<section class="our-philosophy">
 			<div class="container">
 				<div class="col-one">
-					<h1></h1>
-					<h2></h2>
-				</div>
-				<div class="col-two">
-					<p></p>
+					<div class="row-one">
+						<h1>Our philosophy</h1>
+						<h2 class="regular">Individual. Intellectual. Professional</h2>
+					</div>
+					<div class="row-two">
+						<div class="wrapper">
+							<p class="medium">Through our observations and learnings, we have created a logical
+								and sequential Financial Match set of Values and Principles,
+								across Emotional, Intellectual and Physical
+								dimensions that human beings operate in <br><br>
+								Our beliefs are grounded in latest and most advanced scientific
+								research that explore the effects that emotions have on our
+								entire being originating from cells in particular, on our
+								chromosomes and mitochondrial DNA.</p>
+							<div class="links">
+								<div>
+									<a class="medium link">Read More</a>
+									<a>
+										<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H6H8V2V8H6V3.2L2 8L0 6L4.8 2H0V0Z" fill="#41A280"/>
+										</svg>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="col-three">
-					<img src="">
+					<img src="~/static/img/vector-img/office.png">
 				</div>
 			</div>
 		</section>
@@ -72,25 +93,25 @@
 		<section class="done-for-you">
 			<div class="container">
 				<div class="col-one">
-					<h1></h1>
+					<h1>The Done-For-You ERC Service</h1>
 				</div>
 				<div class="col-two">
 					<div class="row-one">
-						<h1></h1>
+						<h2 class="regular">We make <br> the complicated simple</h2>
 					</div>
 					<div class="row-two">
 						<div>
-							<h2></h2>
-							<p></p>
+							<h2 class="regular">ERC regulations</h2>
+							<p class="medium">We make sure all numbers are accurate and fit within ERC regulations so you can “sleep well at night”</p>
 						</div>
 						<div>
-							<h2></h2>
-							<p></p>
+							<h2 class="regular">Experts</h2>
+							<p class="medium">Our ERC experts will take care of the paperwork so you can focus on running your business</p>
 						</div>
-					</div>
-					<div class="row-three">
-						<h2></h2>
-						<p></p>
+						<div class="wrapper">
+							<h2 class="regular">A licensed CPA</h2>
+							<p class="medium">Each account is overseen by a licensed CPA</p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -98,7 +119,68 @@
 
 		<section class="why-us">
 			<div class="container">
+				<h1>Why us?</h1>
+				<div class="column">
+					<div class="desc">
+						<p>We do this by listening and understanding your businesses, identifying your needs, making a plan and holding your hand as we help you utilize government programs, get funding, and optimize your taxes.</p>
+					</div>
+					<div class="item item-a">
+						<div class="wrapper">
+							<h2 class="regular">01</h2>
+							<p class="desc-title">State Licensed CPA's</p>
+						</div>
+						<p class="medium">A certified public accountant (CPA), however, is someone who has earned a professional designation through a combination of education</p>
+					</div>
+					<div class="item item-b">
+						<div class="wrapper">
+							<h2 class="regular">02</h2>
+							<p class="desc-title">Multiple Ways To Quality</p>
+						</div>
+						<p class="medium">The totality of features and characteristics of a product or service that bears on its ability to meet a stated or implied need</p>
+					</div>
+					<div class="item item-c">
+						<div class="wrapper">
+							<h2 class="regular">03</h2>
+							<p class="desc-title">Integrity Over Profit</p>
+						</div>
+						<p class="medium">Integrity in business enhances your reputation — which can help attract and retain customers</p>
+					</div>
+					<div class="item item-d">
+						<div class="wrapper">
+							<h2 class="regular">04</h2>
+							<p class="desc-title">No Upfront Fees</p>
+						</div>
+						<p class="medium">An amount of money paid before a particular piece of work or a particular service is done or received</p>
+					</div>
+					<div class="item item-e">
+						<div class="wrapper">
+							<h2 class="regular">05</h2>
+							<p class="desc-title">Honest & Accurat</p>
+						</div>
+						<p class="medium">A boss who admits to having acted in their own interest may show honesty by rectifying the situation</p>
+					</div>
+				</div>
+			</div>
+		</section>
 
+		<section class="erc-payout">
+			<div class="container">
+				<div class="wrapper">
+					<div class="desc">
+						<h1 class="title">Estimate your ERC Payout</h1>
+						<button>Check eligibility</button>
+					</div>
+					<div class="pay-cost">
+						<div class="wrap">
+							<div class="text-group">
+								<p class="medium">Numbers of Employees:</p>
+								<p>20</p>
+							</div>
+							<input type="range">
+							<h1>$312,000</h1>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 
@@ -334,10 +416,10 @@ export default {
 			grid-template-columns: repeat(4, 1fr);
 			div {
 				display: grid;
-				border: 5px solid var(--grayish);
+				border: 2px solid var(--grayish);
 				max-width: 100%;
 				min-height: 238px;
-				margin: -2px;
+				margin: -1px;
 				align-items: center;
 				img {
 					display: block;
@@ -352,6 +434,164 @@ export default {
 			}
 			.item-c {
 				grid-column: 3;
+			}
+		}
+	}
+	.our-philosophy {
+		padding-bottom: 160px;
+		.container {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+		}
+		.col-one {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			.row-two {
+				display: grid;
+				align-items: end;
+				margin-right: 32px;
+				.links {
+					margin-top: 32px;
+					.link {
+						margin-right: 4px;
+					}
+				}
+			}
+			h2 {
+				color: var(--gray);
+				margin-top: 16px;
+			}
+			a {
+				text-decoration: none;
+				color: var(--lightgreen);
+			}
+		}
+	}
+	.done-for-you {
+		padding-bottom: 160px;
+		.col-one {
+			border-bottom: 2px solid var(--grayish);
+			h1 {
+				max-width: 494px;
+				width: 100%;
+				margin-bottom: 40px;
+			}
+		}
+		.col-two {
+			margin-top: 40px;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			.row-one {
+				h2 {
+					color: var(--gray);
+					max-width: 249px;
+					width: 100%;
+				}
+			}
+			.row-two {
+				display: grid;
+				grid-template-columns: repeat(2, 1fr);
+				grid-gap: 32px;
+				p {
+					margin-top: 8px;
+				}
+			}
+		}
+	}
+	.why-us {
+		padding-bottom: 160px;
+		h1 {
+			margin-bottom: 56px;
+		}
+		.column {
+			display: grid;
+			grid-template-columns: repeat(4, 1fr);
+			.item {
+				padding: 24px;
+				max-width: 346px;
+				border: 2px solid var(--grayish);
+				margin: -1px;
+				.wrapper {
+					display: flex;
+					align-items: flex-end;
+					margin-bottom: 40px;
+					.desc-title {
+						margin-bottom: 3px;
+						margin-left: 24px;
+					}
+				}
+			}
+			.desc {
+				max-width: 346px;
+				min-height: 238px;
+				padding: 24px;
+				border: none;
+				grid-column: 2;
+			}
+			.item-a {
+				grid-row: 2;
+			}
+			.item-b {
+				grid-row: 3;
+				grid-column: 2;
+			}
+			.item-c {
+				grid-row: 3;
+				grid-column: 3;
+			}
+			.item-d {
+				grid-row: 2;
+				grid-column: 3;
+			}
+			.item-e {
+				grid-column: 4;
+			}
+			h2, p.desc-title {
+				color: var(--blue);
+			}
+			p.medium {
+				max-width: 233px;
+				width: 100%;
+				margin-left: auto;
+			}
+		}
+	}
+	.erc-payout {
+		margin-top: 160px;
+		.wrapper {
+			background: no-repeat url(~/static/img/background/payout.png);
+			padding: 32px;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			max-width: 100%;
+		}
+		h1, p {
+			color: var(--white);
+		}
+		.title {
+			max-width: 371px;
+			width: 100%;
+			
+			margin-bottom: 86px;
+		}
+		.pay-cost {
+			display: grid;
+			align-items: end;
+			max-width: 344px;
+			width: 100%;
+			.wrap {
+				height: auto;
+				input {
+					margin-bottom: 14px;
+				}
+				.text-group {
+					display: flex;
+					justify-content: space-between;
+					align-items: center;
+					& p:last-child {
+						margin-bottom: 8px;
+					}
+				}
 			}
 		}
 	}

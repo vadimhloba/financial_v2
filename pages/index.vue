6237c744f4ed7dc3a53969c3385443e52ca9843d
Yosuphew <vadimirua1409@gmail.com>
2023-01-18 10:12:45 +0200

    added some styles

--- pages/index.vue
+++ pages/index.vue
@@ -305,15 +305,48 @@
 			<div class="container">
 				<div class="col-one">
 					<h1>Client review</h1>
-					<div>
-						<a href="#">Next review</a>
-						<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H6H8V2V8H6V3.2L2 8L0 6L4.8 2H0V0Z" fill="#DC5750"/>
-						</svg>
+					<div class="link">
+						<div>
+							<a href="#">Next review</a>
+							<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H6H8V2V8H6V3.2L2 8L0 6L4.8 2H0V0Z" fill="#DC5750"/>
+							</svg>
+						</div>
 					</div>
 				</div>
 				<div class="col-two">
-
+					<div class="row-one">
+						<div class="video">
+							<img src="~/static/img/reviews/review-one.png">
+							<a><svg width="49" height="58" viewBox="0 0 49 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M46.1043 33.3565L8.18522 56.4315C4.9672 58.3877 0.837402 56.019 0.837402 52.0752V5.92522C0.837402 1.98772 4.96124 -0.387279 8.18522 1.57522L46.1043 24.6502C46.8363 25.0885 47.4448 25.722 47.868 26.4865C48.2912 27.251 48.5142 28.1193 48.5142 29.0033C48.5142 29.8874 48.2912 30.7557 47.868 31.5202C47.4448 32.2847 46.8363 32.9182 46.1043 33.3565Z" fill="#FAFAFA"/>
+								</svg></a>
+						</div>
+						<div class="video-desc">
+							<h3>Selena Giri</h3>
+							<p>CEO Chess.com</p>
+							<h2 class="regular">$ 200,500</h2>
+							<p class="mediium">received to our clients</p>
+							<h1>4 months</h1>
+							<p class="medium">from call to receiving money</p>
+						</div>
+					</div>
+					<div class="row-two">
+						<div class="video">
+							<img>
+							<a><svg width="49" height="58" viewBox="0 0 49 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M46.1043 33.3565L8.18522 56.4315C4.9672 58.3877 0.837402 56.019 0.837402 52.0752V5.92522C0.837402 1.98772 4.96124 -0.387279 8.18522 1.57522L46.1043 24.6502C46.8363 25.0885 47.4448 25.722 47.868 26.4865C48.2912 27.251 48.5142 28.1193 48.5142 29.0033C48.5142 29.8874 48.2912 30.7557 47.868 31.5202C47.4448 32.2847 46.8363 32.9182 46.1043 33.3565Z" fill="#FAFAFA"/>
+								</svg></a>
+						</div>
+						<div class="video-desc">
+							<h3>Tom Hardy</h3>
+							<p>CEO Chess.com</p>
+							<h2 class="regular">$ 200,500</h2>
+							<p class="mediium">received to our clients</p>
+							<h1>4 months</h1>
+							<p class="medium">from call to receiving money</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -764,6 +797,26 @@ export default {
 					padding-inline-start: 19px;
 				}
 			}
+		}
+	}
+	.client-review {
+		padding-bottom: 160px;
+		.col-one {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			align-items: center;
+			margin-bottom: 32px;
+			.link {
+				display: grid;
+				grid-template-columns: repeat(4, 1fr);
+				justify-content: end;
+				& div {
+					grid-column: 2;
+				}
+			}
+		}
+		.col-two {
+
 		}
 	}
 }

aff3eb9a5b5d4c8100166a8e00b1567f60acddf0
Max Globa <onyx18121990@gmail.com>
2023-01-27 08:58:58 +0300

    added counter

--- plugins/mg-animation.client.js
+++ plugins/mg-animation.client.js
@@ -1,36 +1,36 @@
 export default ({ app }, inject) => {
   inject('mgAnimation', () => {
-    setTimeout(()=>{
-      const offset = window.scrollY + window.innerHeight * .60
-      const animBlocks = document.querySelectorAll('[data-anim]');
-      console.dir(animBlocks);
-      if(animBlocks.length > 0){
-        animBlocks.forEach(item => {
-          if(item.dataset.parent === "1"){
-            if(offset > item.parentNode.offsetTop + item.offsetTop){
-              item.classList.add(`animate__${item.dataset.anim}`)
-              if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
-              if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
-            }
-          } else if(item.dataset.parent === "3"){
-            if(offset > item.parentNode.parentNode.parentNode.offsetTop + item.offsetTop){
-              item.classList.add(`animate__${item.dataset.anim}`)
-              if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
-              if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
-            }
-          } else {
-            if(offset > item.offsetTop){
-              item.classList.add(`animate__${item.dataset.anim}`)
-              if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
-              if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
-            } else if(item.nodeName === "rect" && offset > item.parentNode.parentNode.offsetTop){
-              item.classList.add(`animate__${item.dataset.anim}`)
-              if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
-              if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
-            }
+    let counter = 0
+    const offset = window.scrollY + window.innerHeight * .60
+    const animBlocks = document.querySelectorAll('[data-anim]');
+    if(animBlocks.length > 0){
+      animBlocks.forEach(item => {
+        ++counter
+        if(item.dataset.parent === "1"){
+          if(offset > item.parentNode.offsetTop + item.offsetTop){
+            item.classList.add(`animate__${item.dataset.anim}`)
+            if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
+            if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
           }
-        })
-      }
-    }, 50);
+        } else if(item.dataset.parent === "3"){
+          if(offset > item.parentNode.parentNode.parentNode.offsetTop + item.offsetTop){
+            item.classList.add(`animate__${item.dataset.anim}`)
+            if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
+            if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
+          }
+        } else {
+          if(offset > item.offsetTop){
+            item.classList.add(`animate__${item.dataset.anim}`)
+            if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
+            if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
+          } else if(item.nodeName === "rect" && offset > item.parentNode.parentNode.offsetTop){
+            item.classList.add(`animate__${item.dataset.anim}`)
+            if(item.dataset.delay) item.style.animationDelay = `${item.dataset.delay}ms`
+            if(item.dataset.duration) item.style.animationDuration = `${item.dataset.duration}ms`
+          }
+        }
+      })
+      console.log(counter)
+    }
   })
 }
